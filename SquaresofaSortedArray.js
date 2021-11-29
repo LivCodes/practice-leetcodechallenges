14066019ad35831fd04dd3940c5ed36141c61801
LivCodes <oliviareeddev@gmail.com>
2021-11-29 01:14:01 -0500

    solution & alternate solution

--- SquaresofaSortedArray.js
+++ SquaresofaSortedArray.js
@@ -21,3 +21,35 @@ Constraints:
 -104 <= nums[i] <= 104
 nums is sorted in non-decreasing order.
  */
+
+const sortedSquares = (nums) => {
+    return nums.map((number, index) => {
+          return number *= number
+      }).sort((a,b) => a - b)
+  
+  };
+
+  // refactored: 
+  // const sortedSquares = (nums) =>  nums.map((number, index) => number *= number).sort((a,b) => a - b);
+
+
+  // 2 pointer method
+  var sortedSquares = function(nums) {
+    var orderedArray = new Array(nums.length);
+    var orderedArrayIndex = orderedArray.length-1;
+    var left = 0;
+    var right = nums.length-1;
+    
+    while(left <= right) {
+        if (Math.abs(nums[left]) <= Math.abs(nums[right])) {
+            orderedArray[orderedArrayIndex] = nums[right]**2;
+            orderedArrayIndex--;
+            right--;
+        } else {
+            orderedArray[orderedArrayIndex] = nums[left]**2;
+            orderedArrayIndex--;
+            left++;
+        }
+    }
+    return orderedArray;
+};
